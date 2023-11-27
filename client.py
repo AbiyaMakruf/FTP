e0b50f06266b61218dcdf9fb512db69604a21637
AbiyaMakruf <aabbiiyyaa@gmail.com>
2023-11-27 18:58:03 +0700

    Add menu, login, download

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import socket
 
-def send_file(file_path):
-    host = "127.0.0.1"
+def unggah(file_path):
+    host = "192.168.18.109"
     port = 12345
 
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -21,6 +21,81 @@ def send_file(file_path):
 
         print("File sent successfully")
 
+def download():
+    host = "127.0.0.1"
+    port = 12345
+
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((host, port))
+
+    with client_socket:
+        # Ask user for request type (list or download)
+        request_type = input("Enter request type (list/download): ")
+        client_socket.send(request_type.encode())
+
+        if request_type == "list":
+            # Receive and print the list of files
+            file_list = client_socket.recv(1024).decode()
+            print("List of files in the database:")
+            print(file_list)
+
+        elif request_type == "download":
+            # Send file name
+            file_name = input("Enter file name: ")
+            client_socket.send(file_name.encode())
+
+            # Receive file existence confirmation
+            response = client_socket.recv(1024).decode()
+
+            if response == "EXISTS":
+                # Open a new file for writing
+                with open(file_name, 'wb') as file:
+                    # Receive file content
+                    data = client_socket.recv(1024)
+                    while data:
+                        file.write(data)
+                        data = client_socket.recv(1024)
+
+                print("File received successfully")
+
+            elif response == "NOT_FOUND":
+                print("File not found")
+
+        else:
+            print("Invalid request type. Please enter 'list' or 'download'.")
+
+
+def login():
+    username = input("Username: ")
+    password = input("Password: ")
+    return username == "admin" and password == "admin"
+
+def menu():
+        print("Login berhasil")
+        print("="*20,"Menu","="*20)
+
+        menu = "0"
+        while menu != "3":
+            print("1. Upload")
+            print("2. Download")
+            print("3. Exit")
+            menu = input("Pilih menu: ")
+            if menu == "1":
+                file_path = str(input("Masukkan path file: "))
+                unggah(file_path)
+            elif menu == "2":
+                download()
+            elif menu == "3":
+                exit()
+            else:
+                print("Menu tidak tersedia\n")
+
+def main():
+    print("="*20,"Selamat Datang","="*20)
+    if login():
+        menu()
+    else:
+        print("Login gagal")
+
 if __name__ == "__main__":
-    file_path = "./Database/file.txt"  # Replace with the path to your file
-    send_file(file_path)
+    main()
--- server.py
+++ server.py
@@ -1,8 +1,10 @@
+import os
 import socket
 
 def start_server():
-    host = "127.0.0.1"
+    host = "192.168.18.109"
     port = 12345
+    database_folder = "./Database"
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind((host, port))
@@ -10,23 +12,82 @@ def start_server():
 
     print(f"Server listening on {host}:{port}")
 
-    conn, addr = server_socket.accept()
-    print(f"Connection from {addr}")
+    while True:
+        conn, addr = server_socket.accept()
+        print(f"Connection from {addr}")
 
-    with conn:
-        # Receive file name
-        file_name = conn.recv(1024).decode()
-        print(f"Receiving file: {file_name}")
+        with conn:
+            # Receive request type (list or download)
+            request_type = conn.recv(1024).decode()
 
-        # Open a new file for writing
-        with open(file_name, 'wb') as file:
-            # Receive file content
-            data = conn.recv(1024)
-            while data:
-                file.write(data)
-                data = conn.recv(1024)
+            if request_type == "list":
+                # Send list of files in the database folder
+                file_list = "\n".join(os.listdir(database_folder))
+                conn.sendall(file_list.encode())
+                print("File list sent successfully")
 
-        print("File received successfully")
+            elif request_type == "download":
+                # Receive file name
+                file_name = conn.recv(1024).decode()
+                print(f"Receiving file: {file_name}")
+
+                # Construct the file path
+                file_path = os.path.join(database_folder, file_name)
+
+                # Check if the file exists
+                if os.path.exists(file_path):
+                    # Send file existence confirmation
+                    conn.sendall("EXISTS".encode())
+
+                    # Open the file and send its content
+                    with open(file_path, 'rb') as file:
+                        data = file.read(1024)
+                        while data:
+                            conn.sendall(data)
+                            data = file.read(1024)
+
+                    print("File sent successfully")
+
+                else:
+                    # Send file not found notification
+                    conn.sendall("NOT_FOUND".encode())
+                    print("File not found")
 
 if __name__ == "__main__":
     start_server()
+
+
+
+# import socket
+
+# def start_server():
+#     host = "127.0.0.1"
+#     port = 12345
+
+#     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     server_socket.bind((host, port))
+#     server_socket.listen(1)
+
+#     print(f"Server listening on {host}:{port}")
+
+#     while True:
+#         conn, addr = server_socket.accept()
+#         print(f"Connection from {addr}")
+
+#         with conn:
+#             # Receive file name
+#             file_name = conn.recv(1024).decode()
+#             print(f"Receiving file: {file_name}")
+
+#             # Open a new file for writing
+#             with open(file_name, 'wb') as file:
+#                 # Receive file content
+#                 data = conn.recv(1024)
+#                 while data:
+#                     file.write(data)
+#                     data = conn.recv(1024)
+
+#             print("File received successfully")
+
+# if __name__ == "__main__":
+#     start_server()
